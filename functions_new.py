37e190f469ea873901bae0e8b092c2b990bfb25a
dominick.cole <dominick.cole@martinrea.com>
2024-08-08 15:18:45 -0400

    added detailed comments

--- functions_new.py
+++ functions_new.py
@@ -2,56 +2,58 @@ from pandas import read_excel
 import os
 
 
-# searches selected directory and extracts stepfiles into list
-def stpFinder(source_dir):
-    stp_files = []
-    extensions = (".stp", ".step", ".STP", ".STEP")
-    for root, dirs, files in os.walk(source_dir):
-        for file in files:
-            if file.endswith(extensions):
-                stp_files.append(os.path.join(root, file))
+# Searches the selected directory and extracts step files into a list
+def stp_finder(source_dir):
+    stp_files = []  # Initialize an empty list to store step files
+    extensions = (".stp", ".step", ".STP", ".STEP")  # Define the files extensions to look for
+    for root, dirs, files in os.walk(source_dir):  # Walk through the directory
+        for file in files:  # Iterate over the files in the directory
+            if file.endswith(extensions):  # Check if the file has a step extension
+                stp_files.append(os.path.join(root, file))  # Add the files to the list
     return stp_files
 
 
-# Reads BOM excel and returns dataframe
-def extractMREBOM(path):
+# Reads the BOM file and returns a pd DataFrame
+def extract_mre_bom(path):
     df = read_excel(path, sheet_name="BOM", dtype="str")
     return df
 
-def checkBOMFormat(df_arg):
+
+# Checks the format of the BOM DataFrame
+def check_bom_format(df_arg):
     des_count = df_arg["DESCRIPTION"].count()
     num_count = df_arg["DOC NUMBER"].count()
     type_count = df_arg["DOC TYPE"].count()
     part_count = df_arg["DOC PART"].count()
 
-    # Checks if BOM is formatted correctly before returning
-    if des_count == num_count and type_count == part_count:
+    # Check if the BOM is formatted correctly
+    if des_count == num_count and type_count == part_count:  # Correct
         return True
-    else:
+    else:  # Incorrect
         return None
 
 
-# Uses returned dataframe to replace string names with BOM Description, then rename
-def stepNameReplace(filepath_arg, df_arg, newName):
+# Uses DataFrame to replace string names with BOM descriptions, then rename the file
+def step_name_replace(filepath_arg, df_arg, new_name):
 
-    output = open(newName, 'w+')
-    file = open(filepath_arg, 'rb')
+    output = open(new_name, 'w+')  # Open new file for writing
+    file = open(filepath_arg, 'rb')  # Open original file for reading
 
-    for row in file:
-        tmp_str = row.decode("utf-8").replace("\r\n", "\r")
+    for row in file:  # Iterate over each row in the file
+        tmp_str = row.decode("utf-8").replace("\r\n", "\r")  # Decode the line and replace line ending
 
         # Perform all replacements for the current line
         for i in range(len(df_arg["DESCRIPTION"])):
             tmp_str = tmp_str.replace(
                 f"{df_arg['DOC NUMBER'][i]}_{df_arg['DOC TYPE'][i].upper()}_{df_arg['DOC PART'][i]}",
-                df_arg['DESCRIPTION'][i])  # case Upper
+                df_arg['DESCRIPTION'][i])  # Replace if upper case
 
             tmp_str = tmp_str.replace(
                 f"{df_arg['DOC NUMBER'][i]}_{df_arg['DOC TYPE'][i].lower()}_{df_arg['DOC PART'][i]}",
-                df_arg['DESCRIPTION'][i])  # case Lower
+                df_arg['DESCRIPTION'][i])  # Replace if lower case
 
         # After all replacements, write the modified line to the output file
         output.write(tmp_str)
-    file.close()
-    output.close()
+    file.close()  # Close the original file
+    output.close()  # Close the output file
     return
--- gui.py
+++ gui.py
@@ -1,9 +1,9 @@
 import PySimpleGUI as sg
 import functions_new as fn
-import sys
 import os
 import threading
 
+# Define a custom theme for the GUI
 my_new_theme = {'BACKGROUND': '#212325',
                 'TEXT': '#F0F0F0',
                 'INPUT': "#343638",
@@ -15,50 +15,53 @@ my_new_theme = {'BACKGROUND': '#212325',
                 'SLIDER_DEPTH': 0,
                 'PROGRESS_DEPTH': 0}
 
+# Add and set the custom theme
 sg.theme_add_new("MyNewTheme", my_new_theme)
 sg.theme("MyNewTheme")
 
+# Set the default options for the GUI
 sg.set_options(font=("Segoe UI Variable", 10))
 
+
+# Function to execute the main processing
 def execute_func(window, values):
     try:
-        # Extracts data from BOM as a panda array
-        df = fn.extractMREBOM((values["excel"]))
+        # Extract the data from the BOM file into a pd DataFrame
+        df = fn.extract_mre_bom((values["excel"]))
 
-        # Checks format of BOM before continuing code-> if format is wrong, err msg
-        BOM_format = fn.checkBOMFormat(df)
+        # Check the format of the BOM file
+        bom_format = fn.check_bom_format(df)
 
-        # If it is correct, the rest of execute_func will run
-        if BOM_format == True:
+        # Proceed if the BOM format is correct
+        if bom_format:  # == True omitted
 
-            # Calls func to search directory and store stepfiles in list
-            files = fn.stpFinder(values["source"])
-            files = [item.replace("\\", "/") for item in files]
+            # Find all step files in the source directory
+            files = fn.stp_finder(values["source"])
+            files = [item.replace("\\", "/") for item in files]  # Normalize the file paths
             print(f"Found {len(files)} .stp files")
             if len(files) <= 0:
                 window.write_event_value("Error", "no_steps_found")
 
-
             total_files = len(files)
 
-            # Create new directory within source directory
+            # Create an output directory within the source directory
             output_dir = os.path.join(values["source"], "SNR_Output")
             os.makedirs(output_dir, exist_ok=True)
 
-            # Iterates over step files and assigns a number to each file
+            # Process each step file
             for index, file in enumerate(files):
 
-                # Creates name for output step file
+                # Create a new name for the output file
                 new_name = os.path.join(output_dir, os.path.basename(file))
 
-                # "Update" assigned to be called by elif to update status bar through each iteration
+                # Update the progress bar
                 window.write_event_value("Update", (index + 1, total_files))
 
-                # Runs stepNameReplace
+                # Replace strings in the step file
                 print(f"Processing file: {file}")
-                fn.stepNameReplace(file, df, new_name)
+                fn.step_name_replace(file, df, new_name)
 
-            # Once loop completes, "Done" value assigned to be called by elif
+            # Indicate that the processing is done
             window.write_event_value("Done", None)
 
         else:
@@ -67,32 +70,34 @@ def execute_func(window, values):
     except Exception as e:
         window.write_event_value("Error", str(e))
 
-# Sets layout for GUI
+
+# Define the layout for the GUI
 layout = [
     [sg.Input(), sg.FolderBrowse("Select Source Directory", key="source", pad=(5, 5))],
     [sg.Input(), sg.FileBrowse("Select BOM", key="excel", pad=(5, 5))],
     [sg.Button("Execute"),
-     sg.ProgressBar(1000, orientation='h', pad=(9, 5) , size=(19, 15), key='progressbar'),
+     sg.ProgressBar(1000, orientation='h', pad=(9, 5), size=(19, 15), key='progressbar'),
      sg.Text("", key="info")]
 ]
 
-# Creates window variable, assigns name and layout
-window = sg.Window(title="Step Name Replace (SNR)", layout=layout)
+# Create the main window with the specified layout
+window_main = sg.Window(title="Step Name Replace (SNR)", layout=layout)
 
-# Main loop
+# Main event loop
 while True:
-    event, values = window.read()
+    event, values = window_main.read()  # Read events and values from the window
 
-    if event == sg.WIN_CLOSED:
+    if event == sg.WIN_CLOSED:  # If the window is closed, exit the loop
         break
 
-    elif event == "Execute":
+    elif event == "Execute":  # If the Execute button is clicked
         print(f"Source Directory: {values['source']}")
         print(f"Source Directory: {values['excel']}")
 
-        threading.Thread(target=execute_func, args=(window, values), daemon=True).start()
+        # Start the execution function in a new thread
+        threading.Thread(target=execute_func, args=(window_main, values), daemon=True).start()
 
-    elif event == "Error":
+    elif event == "Error":  # If an error occurs
         error_message = values[event]
         # print(values[event])
         if "[Errno 13]" in error_message:
@@ -116,14 +121,14 @@ while True:
                      f"Click OK to terminate.")
         break
 
-    elif event == "Update":
+    elif event == "Update":  # Update the progress bar
         current_file, total_files = values[event]
-        window["progressbar"].update(current_file * 1000 / total_files)
-        window["info"].update(f"Working file {current_file} of {total_files}.")
+        window_main["progressbar"].update(current_file * 1000 / total_files)
+        window_main["info"].update(f"Processing file {current_file} of {total_files}.")
 
-    elif event == "Done":
+    elif event == "Done":  # If processing is done
         sg.popup("Process completed successfully!\n",
                  font=("Segoe UI Variable", 10), custom_text="Close")
-        sys.exit()
+        break
 
-window.close()
+window_main.close()  # Close the window
